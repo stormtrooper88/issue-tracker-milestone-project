4647b288a5c27a518a17d9dbb76f3f790dd1cb81
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-06-23 03:36:55 +0000

    removed home app

--- bugs/models.py
+++ bugs/models.py
@@ -4,8 +4,6 @@ from django.db import models
 class Bugs(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.name
