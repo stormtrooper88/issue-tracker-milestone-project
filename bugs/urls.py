cbd45053f653f9591692be445c985d27c95901ee
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-06-29 03:06:51 +0000

    created and formed 'urls.py' file for bugs folder

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
-from .views import all_bugs
+from .views import all_bugs, bug_detail, create_or_edit_bug
 
 urlpatterns = [
     url(r'^$', all_bugs, name='bugs'),
+    url(r'^(?P<pk>\d+)/$', bug_detail, name='bug_detail'),
+    url(r'^new/$', create_or_edit_bug, name='new_bug'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug')
 ]
