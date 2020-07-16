6c4a638a38787bc636af60b84f07a168aba9fe23
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-07-16 11:30:04 +0000

    migrated features app

--- features/models.py
+++ features/models.py
@@ -11,7 +11,7 @@ class Features(models.Model):
     created_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     finished_date = models.DateTimeField(blank=True, null=True)
     tag = models.CharField(max_length=30, blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
 
 
     def __str__(self):
