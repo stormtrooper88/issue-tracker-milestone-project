be7a0d22f2853c2ef2486cb77d276d80a28df268
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-07-31 19:20:57 +0000

    added allowed host

--- tracker/settings.py
+++ tracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'https://tcb-issue-tracker-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'tcb-issue-tracker-app.herokuapp.com']
 
 
 # Application definition
